3eaafdc8ebb9192e4a8bcaa28db99bc9ff7bfb4d
Bob Woznicki <Owner@iMac.attlocal.net>
2017-10-28 12:10:12 -0700

    Add command line arguments

--- make_chart.rb
+++ make_chart.rb
@@ -101,7 +101,21 @@ class Chart
     end
 end
 
-chart = Chart.new(12, ["g1", "g2", "g3"], ["p1", "p2", "p3", "p4", "p5", "p6"])
+days = 0
+guides = []
+clients = []
+for i in 0..ARGV.length
+    arg = ARGV[i]
+    if arg == "--days"
+        days = ARGV[i+1].to_i
+    elsif arg == "--guides"
+        guides = ARGV[i+1].split(",")
+    elsif arg == "--clients"
+        clients = ARGV[i+1].split(",")
+    end
+end
+
+chart = Chart.new(days, guides, clients)
 chart.generate
 puts chart.to_json
 
